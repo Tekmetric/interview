d2c8cd28f60c5e6942dc012c6b5a92d943069ac8
alpergrg <alpergorgulu@gmail.com>
2022-03-26 14:04:49 +0300

    Add initial test data for inventory

--- backend/src/main/resources/database/data.sql
+++ backend/src/main/resources/database/data.sql
@@ -1,16 +1,16 @@
 create table inventory
 (
-    id               bigint       not null
+    id          bigint       not null
         constraint inventory_pkey primary key,
-    inventory_type   varchar(64)  not null,
-    inventory_status varchar(64)  not null,
-    brand            varchar(100) not null,
-    part_name        varchar(100) not null,
-    part_number      varchar(100) not null,
-    quantity         int          not null,
-    created_at       timestamp    not null default CURRENT_TIMESTAMP,
-    updated_at       timestamp,
-    deleted_at       timestamp
+    type        varchar(64)  not null,
+    status      varchar(64)  not null,
+    brand       varchar(100) not null,
+    part_name   varchar(100) not null,
+    part_number varchar(100) not null,
+    quantity    int          not null,
+    created_at  timestamp    not null default CURRENT_TIMESTAMP,
+    updated_at  timestamp,
+    deleted_at timestamp
 );
 
 CREATE
@@ -19,4 +19,6 @@ CREATE
 INDEX idx_part_name ON Inventory (part_name);
 CREATE
 INDEX idx_part_number ON Inventory (part_number);
+
+CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1000 INCREMENT BY 1;
 

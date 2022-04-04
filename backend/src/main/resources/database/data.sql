c52b045d5bb5de7d7bdac2aa91389ddbc1ebec65
florin-hriscu <florin.hriscu.extern@kfzteile24.de>
2022-04-04 11:20:42 +0300

    DEV-add sql scripts

--- backend/src/main/resources/database/data.sql
+++ backend/src/main/resources/database/data.sql
@@ -1,1 +1,15 @@
 -- Provide SQL scripts here
+CREATE TABLE book (
+                    id                   INT           NOT NULL,
+                    title                VARCHAR(50)   NOT NULL,
+                    isbn                 VARCHAR(20)   NOT NULL,
+                    publicationYear      INT           NOT NULL,
+                    publisher            VARCHAR(50)   NOT NULL,
+                    PRIMARY KEY (id)
+);
+
+CREATE TABLE author (
+                      id        INT           NOT NULL,
+                      name      VARCHAR(50)   NOT NULL,
+                      PRIMARY KEY (id)
+);

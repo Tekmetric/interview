036a3e80569db341f6f5fabcdf1770b86043fe1e
Nicholas Song <nikol.song.dev@gmail.com>
2022-05-15 10:01:26 -0700

    easy-to-read tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -26,6 +26,7 @@ describe('App', () => {
   it('renders two character cards (Rick and Morty) after loading', async () => {
     render(appComponent);
     await waitForElementToBeRemoved(() => screen.getByTestId('id-loading-spinner'));
+
     const cards = screen.getAllByRole('ccard');
     expect(cards).toHaveLength(2);
     expect(within(cards[0]).getByText(/Rick Sanchez/i)).toBeInTheDocument();
@@ -35,6 +36,7 @@ describe('App', () => {
   it('renders two pagination controls', async () => {
     render(appComponent);
     await waitForElementToBeRemoved(() => screen.getByTestId('id-loading-spinner'));
+
     const paginations = screen.getAllByLabelText('pagination navigation');
     expect(paginations).toHaveLength(2);
   });
@@ -42,9 +44,11 @@ describe('App', () => {
   it('renders one card (Summer) in the next page', async () => {
     render(appComponent);
     await waitForElementToBeRemoved(() => screen.getByTestId('id-loading-spinner'));
+
     const paginations = screen.getAllByLabelText('pagination navigation');
     fireEvent.click(within(paginations[0]).getByLabelText('Go to page 2'));
     await waitForElementToBeRemoved(() => screen.getByTestId('id-loading-spinner'));
+
     const card = screen.getByRole('ccard');
     expect(card).toBeInTheDocument();
     expect(within(card).getByText(/Summer Smith/i)).toBeInTheDocument();
@@ -53,15 +57,19 @@ describe('App', () => {
   it('searches characters by name, resets search properly', async () => {
     render(appComponent);
     await waitForElementToBeRemoved(screen.getByTestId('id-loading-spinner'));
+
     const searchInput = screen.getByTestId('id-input-character-name');
     fireEvent.change(searchInput, { target: { value: 'Smith' } });
     await waitFor(() => screen.getByTestId('id-loading-spinner'));
+
     let cards = screen.getAllByRole('ccard');
     expect(cards).toHaveLength(2);
     expect(within(cards[0]).getByText(/Morty Smith/i)).toBeInTheDocument();
     expect(within(cards[1]).getByText(/Summer Smith/i)).toBeInTheDocument();
+
     fireEvent.click(screen.getByTestId('id-button-clear-search'));
     await waitForElementToBeRemoved(() => screen.getByTestId('id-loading-spinner'));
+    
     cards = screen.getAllByRole('ccard');
     expect(within(cards[0]).getByText(/Rick Sanchez/i)).toBeInTheDocument();
     expect(within(cards[1]).getByText(/Morty Smith/i)).toBeInTheDocument();

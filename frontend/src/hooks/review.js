5197655ccb25dc347e034df0809c63155a42ec03
Alexandru Alecu <a.marian.alexandru@gmail.com>
2022-07-23 15:40:05 +0300

    Implement pagination

--- frontend/src/hooks/review.js
+++ frontend/src/hooks/review.js
@@ -3,20 +3,28 @@ import { getReviews } from '../api/review';
 
 /**
  * @param {number} page
- * @returns { { reviews: Array<Object> } }
+ * @returns { { reviews: Array<Object>, loading: boolean } }
  */
 export const useReviews = page => {
+	const [loading, setLoading] = useState(false);
 	const [reviews, setReviews] = useState([]);
 
 	useEffect(() => {
 		(async () => {
+			setLoading(true);
+
 			const list = await getReviews(page);
 
+			// simulate 0.5s wait
+			await new Promise(resolve => setTimeout(resolve, 500));
+
 			setReviews(list);
+			setLoading(false);
 		})();
 	}, [page]);
 
 	return {
-		reviews
+		reviews,
+		loading
 	};
 };
--- frontend/src/pages/ReviewList.jsx
+++ frontend/src/pages/ReviewList.jsx
@@ -1,31 +1,52 @@
-import { Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, LinearProgress, Pagination } from '@mui/material';
 import { Container } from '@mui/system';
-import React from 'react';
 import { Disclaimer } from '../components/Disclaimer';
 import { Header } from '../components/Header';
 import { ReviewCard } from '../components/ReviewCard';
 import { useReviews } from '../hooks/review';
 
-const List = () => {
-	const { reviews } = useReviews();
-
+const List = ({ reviews }) => {
 	return reviews.map(review => (
-		<Grid item xs={12} sm={6} md={4} key={review.videoTitle}>
+		<Grid item xs={12} sm={6} md={4} key={review.videoCode}>
 			<ReviewCard {...review} />
 		</Grid>
 	));
 };
 
 export const ReviewList = () => {
+	const [page, setPage] = useState(1);
+	const { reviews, loading } = useReviews(page);
+
 	return (
 		<>
+			{loading && (
+				<LinearProgress
+					sx={{
+						position: 'fixed',
+						top: 0,
+						left: 0,
+						width: '100%',
+						zIndex: 9999
+					}}
+				/>
+			)}
+
 			<Header />
 
-			<Container sx={{ paddingTop: 4, paddingBottom: 4 }}>
+			<Container sx={{ paddingTop: 4, paddingBottom: 20 }}>
 				<Disclaimer />
 
+				<Pagination
+					page={page}
+					onChange={(_event, value) => setPage(value)}
+					count={4}
+					shape="circular"
+					sx={{ marginTop: 2, float: 'right' }}
+				/>
+
 				<Grid container spacing={3}>
-					<List />
+					<List reviews={reviews} />
 				</Grid>
 			</Container>
 		</>

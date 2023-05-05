14f7190e450fd4ffed676b758ccf66c24e7ae324
butiricristian <butiri.cristian@gmail.com>
2023-05-05 10:36:47 +0300

    Add car filters

---
+++ frontend/src/Cars/components/CarsFilters.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem, Grid } from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
+import { CARS } from '../../shared/constants';
+
+export default function CarsFilters() {
+  const brands = CARS.map((car) => ({ key: car.brand.toLowerCase(), value: car.brand }));
+  const colors = CARS.map((car) => ({ key: car.color.toLowerCase(), value: car.color }));
+  const [searchParams, setSearchParams] = useSearchParams({ brand: '', color: '' });
+
+  const handleFilterChange = (key, value) => {
+    searchParams.set(key, value);
+    setSearchParams(searchParams);
+  };
+
+  return (
+    <Grid container spacing={2} sx={{ padding: '2rem 5rem' }}>
+      <Grid item xs={12} md={4} lg={3}>
+        <FormControl fullWidth>
+          <InputLabel id="brand-select-label">Brand</InputLabel>
+          <Select
+            labelId="brand-select-label"
+            value={searchParams.get('brand')}
+            label="Brand"
+            onChange={(e) => handleFilterChange('brand', e.target.value)}>
+            {brands.map((brand) => (
+              <MenuItem key={brand.key} value={brand.key}>
+                {brand.value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} md={4} lg={3}>
+        <FormControl fullWidth>
+          <InputLabel id="color-select-label">Color</InputLabel>
+          <Select
+            labelId="color-select-label"
+            value={searchParams.get('color')}
+            label="Color"
+            onChange={(e) => handleFilterChange('color', e.target.value)}>
+            {colors.map((color) => (
+              <MenuItem key={color.key} value={color.key}>
+                {color.value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} md={3}></Grid>
+    </Grid>
+  );
+}
--- frontend/src/Cars/components/CarsList.jsx
+++ frontend/src/Cars/components/CarsList.jsx
@@ -1,37 +1,68 @@
-import { Card, CardContent, Grid, CardMedia, Typography, CardActions, Button } from '@mui/material';
-import React from 'react';
+import {
+  Card,
+  CardContent,
+  Grid,
+  CardMedia,
+  Typography,
+  CardActions,
+  Button,
+  Box,
+  Pagination
+} from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { CARS } from '../../shared/constants';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { truncate } from '../../shared/helpers';
+import CarsFilters from './CarsFilters';
 //import logo from './logo.svg';
 
 function CarsList() {
-  const cars = CARS;
+  const [cars, setCars] = useState(CARS);
   const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+  const handlePageChange = (event, val) => {
+    setPage(val);
+  };
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const newCars = CARS.filter(
+      (car) =>
+        car.brand.toLowerCase() === searchParams.get('brand') &&
+        car.color.toLowerCase() === searchParams.get('color')
+    );
+    setCars(newCars);
+  }, [searchParams]);
 
   return (
-    <Grid container spacing={2} sx={{ padding: '2rem 5rem' }}>
-      {cars.map((car) => (
-        <Grid item xs={12} md={4} lg={3} key={car.id}>
-          <Card>
-            <CardMedia sx={{ height: 140 }} image={car.url} title="green iguana" />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {car.brand} - {car.model}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {truncate(car.description, 180)}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" onClick={() => navigate(`${car.id}`)}>
-                Details
-              </Button>
-            </CardActions>
-          </Card>
+    <Box sx={{ position: 'relative', height: '100%' }}>
+      <Box display="flex" flexDirection="column" alignItems="center" sx={{ mt: 5 }}>
+        <CarsFilters />
+        <Grid container spacing={2} sx={{ padding: '2rem 5rem' }}>
+          {cars.map((car) => (
+            <Grid item xs={12} md={4} lg={3} key={car.id}>
+              <Card>
+                <CardMedia sx={{ height: 140 }} image={car.url} title="green iguana" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {car.brand} - {car.model}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {truncate(car.description, 180)}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" onClick={() => navigate(`${car.id}`)}>
+                    Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
+        <Pagination count={10} page={page} onChange={handlePageChange} sx={{ mt: 5 }} />
+      </Box>
+    </Box>
   );
 }
 

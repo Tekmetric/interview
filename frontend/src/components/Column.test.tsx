3ec79012bd3c3484f40488d625d4e9d5d09784ce
AlexandruBuhai <alexandruabuhai@gmail.com>
2023-07-17 09:13:20 +0300

    Add test for Column component
    Add better styling for the Dog card.

---
+++ frontend/src/components/Column.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import { screen, render, waitFor } from '@testing-library/react';
+import Column from './Column';
+
+describe('testing Column', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('should render a list of dogs', async () => {
+    render(<Column />);
+    await waitFor(async () => {
+      const dogList = await screen.findByTestId('dog-list');
+
+      expect(dogList).toBeInTheDocument();
+    });
+  });
+
+  it('should not fail rendering', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => {
+          throw new Error('Testing the Error');
+        },
+      })
+    ) as jest.Mock;
+    render(<Column />);
+
+    await waitFor(async () => {
+      const dogList = await screen.queryByText('Dogs');
+      expect(dogList).not.toBeInTheDocument();
+    });
+  });
+});

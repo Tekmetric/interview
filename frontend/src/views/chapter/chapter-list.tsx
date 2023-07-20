52676ad524f50ce1409428da3e1878d5b28bc6f4
cosminbarzan <barzancosmin@yahoo.com>
2023-07-21 01:38:24 +0300

    create the chapter list page

---
+++ frontend/src/views/chapter/chapter-list.tsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from 'react';
+import { GlobalContext } from '../../shared/context/global';
+import { useGetChapters } from '../../shared/hooks/api/use-get-chapters';
+import Chapter from '../../features/chapters/chapter';
+import { ChapterType } from '../../shared/types/chapter';
+import { Typography } from '@mui/material';
+import Loader from '../../components/loader';
+import './chapter-list.css';
+
+const ChapterList = () => {
+  const { globalState } = useContext(GlobalContext);
+  const storageToken = localStorage.getItem('token') ?? '';
+
+  const [chapters, setChapters] = useState<ChapterType[]>();
+
+  const { data, refetch: getChapters, isLoading, isError } = useGetChapters(globalState.token || storageToken);
+
+  useEffect(() => {
+    getChapters();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      console.log('chapters', data);
+      setChapters(data);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <header>
+        <Typography variant="h1" component="h1" className="text-6xl text-center mb-12 py-6 bg-slate-300">
+          Bhagavad Gita Chapters
+        </Typography>
+      </header>
+      <div className="chapter-list-wrapper">
+        {isError && <p>Failed to load chapters</p>}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          chapters?.map((chapter: ChapterType, index: number) => {
+            return (
+              <div key={index}>
+                <Chapter
+                  chapter_number={chapter.chapter_number}
+                  name_meaning={chapter.name_meaning}
+                  chapter_summary={chapter.chapter_summary}
+                />
+              </div>
+            );
+          })
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ChapterList;

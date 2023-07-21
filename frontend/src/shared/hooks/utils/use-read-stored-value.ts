d0f1e74861a219009a5b1205f6063bb143e7d181
cosminbarzan <barzancosmin@yahoo.com>
2023-07-21 03:22:38 +0300

    add generic types for read store value hook

--- frontend/src/shared/hooks/utils/use-read-stored-value.ts
+++ frontend/src/shared/hooks/utils/use-read-stored-value.ts
@@ -1,30 +1,32 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { GlobalContext, GlobalStateType } from '../../context/global';
 
-const useReadStoredValue = (key: string) => {
+type Value<T> = T | null;
+
+function useReadStoredValue<T>(key: string): Value<T> {
   const { globalState } = useContext(GlobalContext);
 
-  const readValue = useCallback(() => {
+  const readValue = useCallback((): Value<T> => {
     try {
       const globalToken = globalState[key as keyof GlobalStateType];
       const storageToken = localStorage.getItem(key);
 
       const token = globalToken || storageToken;
 
-      return token;
+      return token as Value<T>;
     } catch (error) {
       console.warn(`Error reading stored value for key "${key}":`, error);
       return null;
     }
   }, [key, globalState]);
 
-  const [storedValue, setStoredValue] = useState(readValue);
+  const [storedValue, setStoredValue] = useState<Value<T>>(readValue);
 
   useEffect(() => {
     setStoredValue(readValue());
   }, [globalState]);
 
   return storedValue;
-};
+}
 
 export default useReadStoredValue;

8e89c73b37191f02d55979028e064b4e63a6123f
cosminbarzan <barzancosmin@yahoo.com>
2023-07-21 03:58:02 +0300

    save api urls in separate constants

--- frontend/src/shared/hooks/api/use-get-chapters.ts
+++ frontend/src/shared/hooks/api/use-get-chapters.ts
@@ -1,13 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { ChapterType } from '../../types/chapter';
 
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+const apiPath = '/api/v1/chapters?access_token';
+
 export const useGetChapters = (accessToken: string) => {
-  console.log('use-get-chapters');
   return useQuery({
     queryKey: ['query:chapters'],
     queryFn: async () => {
       console.log('fetch');
-      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/v1/chapters?access_token=${accessToken}`);
+      const response = await fetch(`${baseUrl}${apiPath}=${accessToken}`);
 
       if (!response.ok) {
         throw new Error('Failed to fetch chapters list.');
--- frontend/src/shared/hooks/api/use-login-api.ts
+++ frontend/src/shared/hooks/api/use-login-api.ts
@@ -1,13 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { AuthDataType, LoginDataType } from '../../types/login';
 
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+const apiPath = '/auth/oauth/token';
+
 export const useLoginApi = (authData: LoginDataType) => {
   const authDataQueryString = new URLSearchParams(authData).toString();
 
   return useMutation({
     mutationKey: ['mutation:login'],
     mutationFn: async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/auth/oauth/token`, {
+      const response = await fetch(`${baseUrl}${apiPath}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',

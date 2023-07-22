a61357521202ccc2ec2ad5e3916c78c92f6f793f
Vlad Alin <alinvlad94@gmail.com>
2023-07-22 12:04:23 +0300

    Added tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,23 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h2>Welcome to the interview app!</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="w-full flex flex-col justify-center items-center">
+      <h2 className="m-10">Welcome to the interview app!</h2>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
 
-        <ul>
-          <li>
-            Fetch Data from a public API{' '}
-            <a href='https://github.com/toddmotto/public-apis'>Samples</a>
-          </li>
-          <li>Display data from API onto your page (Table, List, etc.)</li>
-          <li>
-            Apply a styling solution of your choice to make your page look different (CSS, SASS,
-            CSS-in-JS)
-          </li>
-        </ul>
-      </header>
+      <ul className="m-10">
+        <li>
+          Fetch Data from a public API{' '}
+          <a href='https://github.com/toddmotto/public-apis'>Samples</a>
+        </li>
+        <li>Display data from API onto your page (Table, List, etc.)</li>
+        <li>
+          Apply a styling solution of your choice to make your page look different (CSS, SASS,
+          CSS-in-JS)
+        </li>
+      </ul>
     </div>
   )
 }

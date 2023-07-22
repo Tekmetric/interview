7cc88530d4c991d9cb7b093776d1d4895c02cda1
Vlad Alin <alinvlad94@gmail.com>
2023-07-22 12:07:50 +0300

    Install Material UI + React Query dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      <h2 className="m-10">Welcome to the interview app!</h2>
+    <div className='w-full flex flex-col justify-center items-center'>
+      <h2 className='m-10'>Welcome to the interview app!</h2>
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
 
-      <ul className="m-10">
+      <ul className='m-10'>
         <li>
           Fetch Data from a public API{' '}
           <a href='https://github.com/toddmotto/public-apis'>Samples</a>

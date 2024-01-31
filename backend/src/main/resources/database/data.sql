8fefba20405cefd9298b03a3bc74e288a1864b8b
Luke Tyler Downey <ltdowney@users.noreply.github.com>
2024-01-30 19:19:44 -0500

    Table schema for basic CRUD records

--- backend/src/main/resources/database/data.sql
+++ backend/src/main/resources/database/data.sql
@@ -1,1 +1,13 @@
--- Provide SQL scripts here
+-- Basic data entity table and related schema
+
+create table if not exists data (
+    id      UUID                     not null,
+
+    name    character varying(100)   not null,
+    count   integer                  not null,
+
+    created timestamp with time zone not null default CURRENT_TIMESTAMP,
+    updated timestamp with time zone not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+
+    constraint pk_data primary key (id)
+);

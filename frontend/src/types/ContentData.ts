0228de20620b26dbc74a9086a743fa1de4817d3c
Radu Baston <radu.baston@sectorlabs.ro>
2024-03-03 12:16:19 +0200

    Add model for representing movies and series

---
+++ frontend/src/types/ContentData.ts
@@ -0,0 +1,14 @@
+export type ContentData = {
+  imdbID: string;
+  posterURL?: string;
+  name: string;
+  description: string;
+  yearStart: number;
+  yearEnd?: number;
+  type: ContentType;
+};
+
+export enum ContentType {
+  MOVIE,
+  SERIES,
+}

6f3b4e27030decd1fab30a6b81b894899fc888bd
Radu Baston <radu.baston@sectorlabs.ro>
2024-03-04 22:56:58 +0200

    Add social media links and remove instagram icon

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,18 @@
+import Link from '@mui/material/Link';
+
 import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => (
   <div className="flex flex-col gap-y-3 justify-center items-center w-screen bg-black max-h-fit py-6">
     <div className="text-white text-xl">Radu Baston</div>
     <div className="flex gap-x-5 justify-center item-center">
-      <GitHubIcon sx={{ color: 'white' }} />
-      <InstagramIcon sx={{ color: 'white' }} />
-      <LinkedInIcon sx={{ color: 'white' }} />
+      <Link href="https://github.com/radu2147">
+        <GitHubIcon sx={{ color: 'white' }} />
+      </Link>
+      <Link href="https://www.linkedin.com/in/radu-baston-a99960185/">
+        <LinkedInIcon sx={{ color: 'white' }} />
+      </Link>
     </div>
   </div>
 );

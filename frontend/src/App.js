560ab93579903a71ce73f3c86cee0e165a9a1301
claire freehafer <22858381+clairefreehafer@users.noreply.github.com>
2024-03-16 13:15:11 -0700

    display asteroid data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { fetchNEOs } from "./utils";
+import NEOItem from "./components/NEOItem";
 
  
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
     this.state = {
       date: new Date(today.getTime() - offset).toISOString().split("T")[0],
       neoData: [],
+      units: "metric",
     }
   }
 
@@ -27,9 +29,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>NEO Viewer</h1>
-          <h2>View by date asteroids that have come within 1.3 AU of Earth.</h2>
+          <h2>View by date asteroids that have come within 1.3 AU* of Earth.</h2>
+          <p>*1 AU = 1 Astronomical Unit ≈ {this.state.units === "metric" ? "149,597,871 km" : "92,955,807 mi"}</p>
         </header>
+        <section>
+          {/* date selection */}
+          {/* unit selection */}
+        </section>
         <main>
+          {this.state.neoData.map((neo) => (
+            <NEOItem key={neo.id} neo={neo} units={this.state.units} />
+          ))}
         </main>
       </div>
     );
---
+++ frontend/src/components/NEOItem.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+export default function NEOItem({ neo, units }) {
+  const [estimatedDiameter, setEstimatedDiameter] = useState({ min: "", max: "", unit: "" });
+
+  const {
+    close_approach_data: closeApproachData,
+    estimated_diameter,
+  } = neo;
+
+  useEffect(() => {
+    let key = "";
+    let unit = "";
+
+    if (units === "metric") {
+      // use kilometers if asteroid is larger than 1km
+      if (estimated_diameter.kilometers.estimated_diameter_max > 1) {
+        key = "kilometers";
+        unit = "km";
+      } else {
+        key = "meters";
+        unit = "m";
+      }
+    } else {
+      // use miles if asteroid is larger than 1mi
+      if (estimated_diameter.miles.estimated_diameter_max > 1) {
+        key = "miles";
+        unit = "mi";
+      } else {
+        unit = "feet";
+        unit = "ft";
+      }
+    }
+
+    setEstimatedDiameter({
+      min: estimated_diameter[key].estimated_diameter_min.toLocaleString(),
+      max: estimated_diameter[key].estimated_diameter_max.toLocaleString(),
+      unit,
+    });
+
+  }, [units]);
+
+  return (
+    <div>
+      <h3>{neo.is_potentially_hazardous_asteroid && "⚠️"} {neo.name}</h3>
+      <p>
+        <strong>size</strong>:&nbsp;
+        {estimatedDiameter.min} - {estimatedDiameter.max} {estimatedDiameter.unit}
+      </p>
+      <p>
+        <strong>closest distance</strong>:&nbsp;
+        {units === "metric"
+          ? `${Number(closeApproachData[0].miss_distance.kilometers).toLocaleString()} km`
+          : `${Number(closeApproachData[0].miss_distance.miles).toLocaleString()} mi`}
+      </p>
+      <p>
+        <strong>relative velocity</strong>:&nbsp;
+        {units === "metric"
+          ? `${Number(closeApproachData[0].relative_velocity.kilometers_per_hour).toLocaleString()} kph`
+          : `${Number(closeApproachData[0].relative_velocity.miles_per_hour).toLocaleString()} mph`}
+      </p>
+    </div>
+  )
+}

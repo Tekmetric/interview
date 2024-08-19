7e5a19f6b84fb051271c3c950fe05069f900dcf7
Michael Foss <michael@mikefoss.com>
2024-08-19 04:40:12 -0400

    Add card fetching library

---
+++ frontend/src/api/consts.js
@@ -0,0 +1,6 @@
+// We need to add a delay to the API fetches
+// or we'll be denied service; a small price
+// to pay for free API access I guess :)
+const FETCH_DELAY = 200;
+
+export { FETCH_DELAY };

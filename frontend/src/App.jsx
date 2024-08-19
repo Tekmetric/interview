3ae10cbf1935a6b1b824703d2cfc07cfa0b7b5c6
Michael Foss <michael@mikefoss.com>
2024-08-19 04:59:47 -0400

    Bootstrap app
    Load card sets on app load. Show card sets in select element.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useBootstrap } from './hooks/useBootstrap';
 
 import './App.css';
 
 const App = () => {
+  const cardSetRef = useRef(null);
+  const [cardSetCode, setCardSetCode] = useState('');
+  const { isBootstrapping, bootstrapError, cardSets } = useBootstrap();
+
+  useEffect(
+    () => {
+      const handleBootstrapCompleted = () => {
+        if (cardSets.length > 0) {
+          setCardSetCode(cardSets[0].code);
+          if (cardSetRef.current) {
+            cardSetRef.current.focus();
+          }
+        }
+      };
+
+      if (!isBootstrapping && !bootstrapError) {
+        handleBootstrapCompleted();
+      }
+    },
+    [isBootstrapping, bootstrapError],
+  );
+
+  const handleCardSetChange = e => {
+    const newCardSetCode = e.target.value;
+    if (newCardSetCode !== cardSetCode) {
+      setSelectedCardIndex(undefined);
+      setCardSetCode(newCardSetCode);
+    }
+  };
+
+  if (isBootstrapping) {
+    return <div>Bootstrapping the app...</div>;
+  }
+
+  if (bootstrapError) {
+    return <div>Error: {bootstrapError.message}</div>;
+  }
+
   return (
     <div className="app">
       <main className="pageWrapper">
         <h1 className="title">Magic The Gathering Booster Pack Generator</h1>
         <div className="content">
-          <div className="leftSide" />
+          <div className="leftSide">
+            <div>
+              <label htmlFor="cardSet">Set: </label>
+              <select
+                id="cardSet"
+                ref={cardSetRef}
+                onChange={handleCardSetChange}
+                value={cardSetCode}
+              >
+                {cardSets.map(cardSet => (
+                  <option key={cardSet.code} value={cardSet.code}>
+                    {cardSet.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
           <div className="rightSide" />
         </div>
       </main>

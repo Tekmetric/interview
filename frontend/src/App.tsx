3d95c8166c1554f7c3110cd347aa6a241682c91e
norbert-bc <norbert.bereczki@icloud.com>
2024-08-24 11:32:08 +0300

    Fix yarn, add typescript, add react-router-dom and bump versions

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+//import logo from './logo.svg';
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to the interview app!</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload. With typescript
+        </p>
+
+      <ul>
+        <li>Fetch Data from a public API <a href="https://github.com/toddmotto/public-apis">Samples</a></li>
+        <li>Display data from API onto your page (Table, List, etc.)</li>
+        <li>Apply a styling solution of your choice to make your page look different (CSS, SASS, CSS-in-JS)</li> 
+      </ul>   
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;

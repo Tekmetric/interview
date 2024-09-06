f412c9ebfb3c39fbc7a5bb705c895fc0bcfeb91a
Caleb Heinz <calebheinz+1@gmail.com>
2024-09-05 21:32:09 -0400

    Uploading data on startup. A new property was introduced in SB 2.5 that affected this script.

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TEAMS (name, city, num_wins, num_losses, created_at, updated_at) VALUES
+    ('Red Sox', 'Boston', 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Yankees', 'New York City', 3, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Orioles', 'Baltimore', 5, 6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Rays', 'Tampa Bay', 7, 8, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Blue Jays', 'Toronto', 9, 10, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);

82be37b3a635c543df090b78eaaa1d783fd9bd72
Catrinel Carausu <cc@remsdaq.com>
2024-09-21 14:05:02 +0300

    init app: vite + react 18 + vitest

---
+++ frontend/src/test/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { describe, expect, test } from "vitest";
+import { render, screen} from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  test("renders", () => {
+    render(<App />);
+    expect(screen.getByText("Vite + React")).toBeDefined();
+  });
+});

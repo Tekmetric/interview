6bcbeb405d097370df3e7707c7661f331c322f85
sanex <sanex3339@yandex.ru>
2024-11-01 16:30:16 +0400

    Fix nested svg

--- frontend/src/components/Spinner/styles.ts
+++ frontend/src/components/Spinner/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
-export const StyledSpinner = styled.svg`
-    width: 48px;
-    height: 48px;
-    color: var(--color-main);
+export const StyledSpinner = styled.div`
+    & svg {
+        width: 48px;
+        height: 48px;
+        color: var(--color-main);
+    }
 `

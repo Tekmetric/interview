45f761f98bf9af73606b553a4f8de50b440e7e55
Andrei Mocanu <andreimcornel@gmail.com>
2024-11-04 13:16:15 +0200

    chore: hide `ReactQueryDevtools` in non prod environments

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,14 +4,23 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 2,
+			staleTime: 1000 * 60 * 5,
+		},
+	},
+});
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Component {...pageProps} />
 
-			<ReactQueryDevtools initialIsOpen={false} />
+			{process.env.NODE_ENV !== 'production' && (
+				<ReactQueryDevtools initialIsOpen={false} />
+			)}
 		</QueryClientProvider>
 	);
 }

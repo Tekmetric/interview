0c0ae3cb79bc00324c8366c31a6d7ea3672ee908
Jake Kenneally <jakekenneally@gmail.com>
2025-02-09 18:53:57 -0500

    chore: upgrade dependencies and migrate to latest React
    - used `corepack` to install `yarn`, and `yarn` inserted the `packageManager` line
    - upgraded dependencies with `yarn`
    - fixed `index.js` to work with the latest version of React and ReactDOM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App'; 
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
- 
+const rootElement = createRoot(document.getElementById('root'));
+rootElement.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

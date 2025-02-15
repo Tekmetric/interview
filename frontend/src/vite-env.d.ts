ca59c84bae7ac2fbcd6b5c58c2c5c413958a3886
Jake Kenneally <jakekenneally@gmail.com>
2025-02-15 14:55:52 -0500

    fix: move types in tsconfig to vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vitest/importMeta" />
+/// <reference types="vitest/globals" />
 
 interface ImportMetaEnv {
   readonly VITE_WMATA_API_KEY: string;

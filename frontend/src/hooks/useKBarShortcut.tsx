193311da7bcfc89ae5302f81a25f1095867d300c
Jake Kenneally <jakekenneally@gmail.com>
2025-02-17 22:04:32 -0500

    fix: issue with useKBarShortcut hook with setting state first time

--- frontend/src/hooks/useKBarShortcut.tsx
+++ frontend/src/hooks/useKBarShortcut.tsx
@@ -1,11 +1,25 @@
-import { bindKeyCombo } from '@rwh/react-keystrokes';
-import { useState } from 'react';
+import { bindKeyCombo, KeystrokesContext, unbindKeyCombo } from '@rwh/react-keystrokes';
+import { useContext, useEffect, useState } from 'react';
 
+// Based on https://github.com/RobertWHurst/Keystrokes/blob/master/packages/react-keystrokes/src/use-key-combo.ts
+// but I needed a custom handler and ability to set the state outside the hook.
 export function useKBarShortcut(): [boolean, (show: boolean) => void] {
   const [showKBar, setShowKBar] = useState(false);
-  bindKeyCombo('meta + k', () => setShowKBar(true));
-  bindKeyCombo('control + k', () => setShowKBar(true));
-  bindKeyCombo('escape', () => setShowKBar(false));
+  const keystrokes = useContext(KeystrokesContext)();
+
+  const updateKBarPressedEffect = () => {
+    const showKBar = () => setShowKBar(true);
+    const hideKBar = () => setShowKBar(false);
+    bindKeyCombo('meta + k', showKBar);
+    bindKeyCombo('control + k', showKBar);
+    bindKeyCombo('escape', hideKBar);
+    return () => {
+      unbindKeyCombo('meta + k', showKBar);
+      unbindKeyCombo('control + k', showKBar);
+      unbindKeyCombo('escape', hideKBar);
+    };
+  };
+  useEffect(updateKBarPressedEffect, [keystrokes]);
 
   return [showKBar, setShowKBar];
 }

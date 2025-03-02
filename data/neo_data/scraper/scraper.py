0d4832860d3b30aeaceabc0aaeb232f2ad3a0d4a
Mihai Campean <sensei@androidsensei.com>
2025-03-02 16:10:38 +0200

    added docstring to scraper

--- data/neo_data/scraper/scraper.py
+++ data/neo_data/scraper/scraper.py
@@ -12,6 +12,13 @@ class Scraper:
         self.data_handler = data_handler
 
     def scrape(self):
+        """
+        Scrapes data by starting two threads: one for downloading data and one for processing data.
+        This method initializes and starts two threads:
+        - The producer thread, which is responsible for downloading data.
+        - The consumer thread, which is responsible for processing the downloaded data.
+        The method logs the start of each thread and waits for both threads to complete before logging that the data scraping is complete.
+        """
         producer = threading.Thread(target=self.data_handler.download_data, name="producer")
         consumer = threading.Thread(target=self.data_processor.run_processing, name="consumer")
 

c5d8de8288f758ec4fe503ef1e4f44cda113a64f
Arni <me@arni.ro>
2025-04-12 12:44:27 +0300

    Handle missing env var gracefully and call neoapi to check if it works

--- data/src/neo_data_analyser/__init__.py
+++ data/src/neo_data_analyser/__init__.py
@@ -1,2 +1,28 @@
+import asyncio
+import sys
+
+import structlog
+from pydantic import ValidationError
+
+from neo_data_analyser.config import get_settings
+from neo_data_analyser.neo_api import NeoApi
+
+logger = structlog.get_logger()
+
+
 def main() -> None:
-    pass
+    asyncio.run(_main())
+
+
+async def _main() -> None:
+    try:
+        get_settings()
+    except ValidationError as exc:
+        logger.error(  # noqa: TRY400
+            "Environment is not configured properly.",
+            reason=[(error["type"], error["loc"]) for error in exc.errors()],
+        )
+        sys.exit(1)
+
+    async with NeoApi() as api:
+        await api.browse()

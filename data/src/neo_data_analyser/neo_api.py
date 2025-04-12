e01257a9129989a8a2076f6e514aec6c5bbbb60c
Arni <me@arni.ro>
2025-04-12 14:30:19 +0300

    Return models from the NeoAPI

--- data/src/neo_data_analyser/neo_api.py
+++ data/src/neo_data_analyser/neo_api.py
@@ -1,10 +1,16 @@
 from http import HTTPStatus
 from types import TracebackType
-from typing import cast
 
 import aiohttp
+import structlog
 
+from neo_data_analyser.models import NearEarthObject
 from neo_data_analyser.settings import get_settings
+
+# The number of items per page for the Neo API. It is a constant and cannot be changed.
+BROWSE_ITEMS_PER_PAGE = 20
+
+logger = structlog.get_logger()
 
 
 class NeoApi:
@@ -15,9 +21,7 @@ class NeoApi:
         self._session: aiohttp.ClientSession
 
     async def __aenter__(self) -> "NeoApi":
-        self._session = aiohttp.ClientSession(
-            headers={"Content-Type": "application/json"}
-        )
+        self._session = aiohttp.ClientSession(headers={"Content-Type": "application/json"})
         return self
 
     async def __aexit__(
@@ -35,16 +39,27 @@ class NeoApi:
         return f"{self._api_url}/{endpoint}?api_key={self._api_key}"
 
     # TODO: Add proper type to the response
-    async def browse(self, page: int = 0) -> dict[str, str]:
+    async def browse(self, page: int = 0) -> list[NearEarthObject]:
+        logger.info(
+            "Fetching data from Neo API",
+            endpoint="browse",
+            page=page,
+        )
         url = self._build_url("browse")
         params = {"page": page}
 
         async with self._session.get(url, params=params) as response:
             if response.status != HTTPStatus.OK:
                 # TODO: Raise custom exception
-                exception_message = (
-                    f"Failed to fetch data from Neo API: {response.status}"
-                )
+                exception_message = f"Failed to fetch data from Neo API: {response.status}"
                 raise RuntimeError(exception_message)
+
             data = await response.json()
-            return cast("dict[str, str]", data)
+
+            logger.info(
+                "Data fetched from Neo API",
+                endpoint="browse",
+                page=page,
+            )
+
+            return [NearEarthObject.model_validate(item) for item in data["near_earth_objects"]]

7474845730dc80a9eaa586587eb2ab2d5481cabd
Arni <me@arni.ro>
2025-04-12 12:43:35 +0300

    Use pydantic settings to validate environment variables

---
+++ data/src/neo_data_analyser/config.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict()
+    neo_api_key: str = Field(alias="NEO_API_KEY")
+    neo_api_url: str = Field(alias="NEO_API_URL")
+
+
+@lru_cache(1)
+def get_settings() -> Settings:
+    return Settings()

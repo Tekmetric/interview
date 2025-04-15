c5294f2844260638cf0a40dbd74f48b0fb6d5ac6
ericcallan <4450043+ericcallan@users.noreply.github.com>
2025-04-15 13:19:55 -0400

    Chore: Removed TO DO in app.tsx in favor of README

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,41 +6,6 @@ import VehicleList from './features/vehicles/vehiclesList/VehicleList';
 import VehicleDetail from './features/vehicles/vehicleForm';
 import Dashboard from './features/application/Dashboard';
 
-/**
- * TO DO
- * FRONTEND
- *** Testing
- *** Bubble Up Backend validations inline to form
- *** Migration to RTK / RTK Query
- *** Lazy Loading
- *** API caching
- *** Review loading state(s)
- *** Review error state(s)
- *** Form skeleton
- *** E2E testing
- *
- * BACKEND
- *** Testing
- *** DTOs
- *** Enhanced Vehicle Validation(s)
- *** S3 Image Uploads
- *
- * DONE
- * Pagination
- * Routing
- * Global Error Handling
- * Search
- * Skeleton Loading
- * Mobile First
- * CRUD: Optimistic Deletes
- * CRUD: Create
- * CRUD: Reads (List Vehicles)
- * CRUD: Read (Form population)
- * CRUD: Update (Form Population)
- * CRUD: Optimistic Deletes
- * FE Snapshot Testing
- */
-
 const App: React.FC = () => {
   return (
     <Router>

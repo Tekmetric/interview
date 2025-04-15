763d9b3a8a08909aa4f014f39aa29d2fbd975b36
ericcallan <4450043+ericcallan@users.noreply.github.com>
2025-04-15 14:51:00 -0400

    Chore: Updated SQL for real car / model associations, added car image loader

--- frontend/src/components/VehicleFormHeader/VehicleFormHeader.tsx
+++ frontend/src/components/VehicleFormHeader/VehicleFormHeader.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react';
+import type { FC } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaArrowLeft, FaTrash } from 'react-icons/fa';
+import { FaArrowLeft, FaTrash, FaSpinner } from 'react-icons/fa';
+
+import { VehicleFormHeaderProps } from '../../features/vehicles/types';
+
 import CarPlaceHolder from '../../assets/images/car_placeholder.png';
 import styles from './vehicleFormHeader.module.css';
-import { VehicleFormHeaderProps } from '../../features/vehicles/types';
 
 const VehicleFormHeader: FC<VehicleFormHeaderProps> = ({
   isEditMode,
@@ -12,31 +15,50 @@ const VehicleFormHeader: FC<VehicleFormHeaderProps> = ({
   vehicleTitle,
   imageUrl,
   onDeleteImage,
-}) => (
-  <div className={styles.header}>
-    <div className={styles.text}>
-      <div className={styles.buttons}>
-        <Link className="link-button" to={`/`}>
-          <FaArrowLeft />
-          <span>Vehicles</span>
-        </Link>
-        {isEditMode && (
-          <button type="button" className={isDisabled ? styles.active : ''} onClick={onEditClick}>
-            Edit
+}) => {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.text}>
+        <div className={styles.buttons}>
+          <Link className="link-button" to={`/`}>
+            <FaArrowLeft />
+            <span>Vehicles</span>
+          </Link>
+          {isEditMode && (
+            <button type="button" className={isDisabled ? styles.active : ''} onClick={onEditClick}>
+              Edit
+            </button>
+          )}
+        </div>
+        <h1>{vehicleTitle}</h1>
+      </div>
+      <div className={styles.image}>
+        {imageUrl && !isDisabled && (
+          <button onClick={onDeleteImage} aria-label="delete image">
+            <FaTrash aria-hidden="true" />
           </button>
         )}
+        <div>
+          <div className="image-loading-overlay">
+            {loading && (
+              <div className={styles.loading}>
+                <FaSpinner />
+              </div>
+            )}
+
+            <img
+              onLoad={() => setLoading(false)}
+              onError={() => setLoading(false)}
+              src={imageUrl || CarPlaceHolder}
+              alt="Vehicle Preview"
+            />
+          </div>
+        </div>
       </div>
-      <h1>{vehicleTitle}</h1>
     </div>
-    <div className={styles.image}>
-      {imageUrl && !isDisabled && (
-        <button onClick={onDeleteImage} aria-label="delete image">
-          <FaTrash aria-hidden="true" />
-        </button>
-      )}
-      <img src={imageUrl || CarPlaceHolder} alt="Vehicle Preview" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default VehicleFormHeader;

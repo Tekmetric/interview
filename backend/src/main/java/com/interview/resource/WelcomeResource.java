ccbbc6198ee1927b07a69940e85d3d24570adf58
svisan <svisan@grubhub.com>
2025-06-09 15:37:28 +0300

    Added lombok

--- backend/src/main/java/com/interview/resource/WelcomeResource.java
+++ backend/src/main/java/com/interview/resource/WelcomeResource.java
@@ -1,8 +1,10 @@
 package com.interview.resource;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class WelcomeResource {
 

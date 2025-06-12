1a7883ecaec5df5f175b48a62be4e1eb53462693
emre.akkaya <emre.akkaya@avasure.com>
2025-06-12 20:16:10 +0300

    fix: Some minor fixes regarding disabling h2 auto-uppercase feature, enabling globally quoted identifiers to overcome any reserved keyword problems and adding optional config prop to disable securityconfig so that it can be disabled during some testing

--- backend/src/main/java/com/interview/config/SecurityConfig.java
+++ backend/src/main/java/com/interview/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.interview.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@ConditionalOnProperty(
+    name = {"spring.security.enabled"},
+    havingValue = "true",
+    matchIfMissing = true
+)
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {

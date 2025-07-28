133060fb00492de8aaa00488e2ba9baa088c66cf
Roman Cherhukhin <xandmore3@gmail.com>
2025-07-28 14:29:46 +0200

    feat: update types for `fetchTrendingMovies` function

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,3 +1,5 @@
+import { Movie } from "./types";
+
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/";
 const API_BASE_URL = "https://api.themoviedb.org/3/";
 
@@ -23,7 +25,14 @@ const api = (path: string, options?: RequestInit) => {
   });
 };
 
-export const fetchTrendingMovies = async (page: number = 1) => {
+export const fetchTrendingMovies = async (
+  page: number = 1
+): Promise<{
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}> => {
   const response = await api(`trending/movie/day?language=en-US&page=${page}`);
 
   if (!response.ok) {

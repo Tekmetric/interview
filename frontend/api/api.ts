1d0f45e34e999c612b57547ecee33abc12bd7bd5
Roman Cherhukhin <xandmore3@gmail.com>
2025-07-28 13:00:03 +0200

    feat: set up API utils

---
+++ frontend/api/api.ts
@@ -0,0 +1,38 @@
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/";
+const API_BASE_URL = "https://api.themoviedb.org/3/";
+
+export type PosterSizes =
+  | "w92"
+  | "w154"
+  | "w185"
+  | "w342"
+  | "w500"
+  | "w780"
+  | "original";
+
+const api = (path: string, options?: RequestInit) => {
+  const { headers, ...rest } = options ?? {};
+
+  return fetch(`${API_BASE_URL}${path}`, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+      ...headers,
+    },
+    ...rest,
+  });
+};
+
+export const fetchTrendingMovies = async (page: number = 1) => {
+  const response = await api(`trending/movie/day?language=en-US&page=${page}`);
+
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching trending movies");
+  }
+
+  return await response.json();
+};
+
+export const getPosterImageUrl = (img: string, size: PosterSizes) => {
+  return `${IMG_BASE_URL}${size}${img}`;
+};

86e8d37187fb88ebaa8132f07cb770c3a54e6179
ziyangzhang <ziyang.zhang@redbird.io>
2025-08-11 22:52:23 -0500

    feat: add flyway and update java/spring version

---
+++ backend/src/main/resources/db/migration/V1__initial_migration.sql
@@ -0,0 +1,13 @@
+-- Drops the table if it already exists (helps in-memory reset)
+DROP TABLE IF EXISTS customer;
+
+CREATE TABLE customer (
+    id UUID PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(25) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_customer_email ON customer(email);
---
+++ backend/src/main/resources/db/migration/V2__add_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, first_name, last_name, email, password) VALUES
+(RANDOM_UUID(), 'Cole', 'Palmer', 'x@example.com', 'password'),
+(RANDOM_UUID(), null, 'Becker', 'y@example.com', 'password'),
+(RANDOM_UUID(), 'Bukayo', 'Saka', 'z@example.com', 'password'),
+(RANDOM_UUID(), 'Mo', 'Salah', 'a@example.com', 'password');

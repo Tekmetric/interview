e0741d5c06a86f542e117a3f62cdcc9d844dca5f
mattjhoughton <mattjhoughton@gmail.com>
2025-09-01 11:45:31 +0100

    simple loader and error handling for rocket data. Updated to functional component in the main App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Sky from "./components/sky";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Sky />
-      </div>
-    );
-  }
+import Loader from "./components/loader";
+import { useRockets } from "./hooks/useRockets";
+
+function App() {
+  const { loading, error } = useRockets();
+
+  return (
+    <div className="App">
+      <Sky />
+      {loading && <Loader text="Fetching Rockets..." />}
+      {error && <div>{error}</div>}
+    </div>
+  );
 }
 
 export default App;

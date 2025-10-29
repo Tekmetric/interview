0448b34329cf9fd1e24aebb5652c3ace9d6da1b7
Kevin Hall <kevinsasa06@gmail.com>
2025-10-28 21:15:37 -0400

    Add more tests for leagueControllerTest

--- backend/src/test/java/com/interview/controller/ControllerTest.java
+++ backend/src/test/java/com/interview/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package com.interview.controller;
 
+import com.interview.model.Team;
 import com.interview.model.dto.TeamDTO;
 import com.interview.model.League;
 import org.junit.jupiter.api.Assertions;
@@ -9,9 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,7 +32,7 @@ public class ControllerTest {
                 .method(HttpMethod.POST, "/api/leagues")
                 .body(league);
         ResponseEntity<League> respPost = rest.exchange("/api/leagues", HttpMethod.POST, requestEntity, League.class);
-        Assertions.assertTrue(respPost.getStatusCode().is2xxSuccessful());
+        Assertions.assertTrue(respPost.getStatusCode().isSameCodeAs(HttpStatus.CREATED));
         Assertions.assertNotNull(respPost.getBody());
         League leagueResponse1 = respPost.getBody();
         Assertions.assertEquals(1, respPost.getBody().getId());
@@ -40,6 +43,9 @@ public class ControllerTest {
         Assertions.assertNull(leagueResponse1.getTeams());
         System.out.println(respPost.getBody().toString());
 
+        ResponseEntity<String> respPostAgain = rest.exchange("/api/leagues", HttpMethod.POST, requestEntity, String.class);
+        Assertions.assertEquals(HttpStatus.CONFLICT, respPostAgain.getStatusCode());
+        Assertions.assertEquals("A conflict has occurred in the database: Row already exists with same name, location, and skill level", respPostAgain.getBody());
 
         ResponseEntity<List<League>> respGetAll = rest.exchange("/api/leagues", HttpMethod.GET, null, new ParameterizedTypeReference<List<League>>() {
         });
@@ -69,12 +75,23 @@ public class ControllerTest {
         Assertions.assertEquals("Location1Update", leagueResponseUpdate.getLocation());
         Assertions.assertEquals("Skill1Update", leagueResponseUpdate.getSkillLevel());
 
+        List<Team> teams = new ArrayList<>();
+        Team team = new Team();
+        team.setName("testTeam");
+        team.setPlayers("Kevin Hall");
+        teams.add(team);
+        League leagueUpdateAgain = new League(null, "League1TestUpdate", "Location1Update", "Skill1Update", teams);
+        RequestEntity<League> requestEntityUpdateAgain = RequestEntity
+                .method(HttpMethod.PUT, "/api/leagues/{id}")
+                .body(leagueUpdateAgain);
+
+        ResponseEntity<String> respUpdateAgain = rest.exchange("/api/leagues/{id}", HttpMethod.PUT, requestEntityUpdateAgain, new ParameterizedTypeReference<>() {}, 1);
+        Assertions.assertTrue(respUpdateAgain.getStatusCode().isSameCodeAs(HttpStatus.BAD_REQUEST));
+        Assertions.assertEquals("Missing required data: Team id is required for league to be updated", respUpdateAgain.getBody());
+
         ResponseEntity<List<TeamDTO>> allTeams = rest.exchange("/api/teams", HttpMethod.GET, null, new ParameterizedTypeReference<List<TeamDTO>>() {
         });
         Assertions.assertNotNull(allTeams);
-
-        ResponseEntity<?> responseEntityLeagueGet= leagueController.getByName("League1TestUpdate");
-        League leagueSingleGetAuto = (League) responseEntityLeagueGet.getBody();
 
         League leagueSingleGet = rest.getForObject("/api/leagues/{id}", League.class, 1);
         Assertions.assertNotNull(leagueSingleGet);
@@ -97,6 +114,11 @@ public class ControllerTest {
 
         ResponseEntity<List<League>> respGetAll2 = rest.exchange("/api/leagues", HttpMethod.GET, null, new ParameterizedTypeReference<List<League>>() {
         });
+        Assertions.assertNotNull(respGetAll2.getBody());
         Assertions.assertTrue(respGetAll2.getBody().isEmpty());
+
+        ResponseEntity<?> respGetByName = rest.exchange("/api/leagues/byName/{name}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, "League1");
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, respGetByName.getStatusCode());
+        Assertions.assertEquals("Row not found: No League Found for name: League1", respGetByName.getBody());
     }
 }

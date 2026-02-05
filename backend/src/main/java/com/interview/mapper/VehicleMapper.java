6c5d747a1bb1442e2a2955232f4301ef072ee9d4
jacktmcg <jacktmcginnis@gmail.com>
2026-02-05 15:37:41 -0500

    remove unused dto

--- backend/src/main/java/com/interview/mapper/VehicleMapper.java
+++ backend/src/main/java/com/interview/mapper/VehicleMapper.java
@@ -1,7 +1,6 @@
 package com.interview.mapper;
 
 import com.interview.model.dto.VehicleDTO;
-import com.interview.model.dto.VehicleServiceOrdersDTO;
 import com.interview.model.entity.Vehicle;
 import org.mapstruct.*;
 
@@ -10,5 +9,4 @@ public interface VehicleMapper {
     @Mapping(target = "customer", ignore = true)
     Vehicle toEntity(VehicleDTO dto);
     VehicleDTO toDTO(Vehicle vehicle);
-    VehicleServiceOrdersDTO toServiceOrdersDTO(Vehicle vehicle);
 }

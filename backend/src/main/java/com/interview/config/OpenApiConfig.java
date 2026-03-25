aef3c87d914f9942fc93737d83695ebd71c29092
Razvan Boldor <razvan.boldor@electrolux.com>
2026-03-25 20:11:26 +0200

    CarShop - Defined API specifications

---
+++ backend/src/main/java/com/interview/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.interview.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI carShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CarShop API")
+                        .version("v1")
+                        .description("Car CRUD management API for the CarShop application. Base path: /carshop/v1/cars"));
+    }
+}
